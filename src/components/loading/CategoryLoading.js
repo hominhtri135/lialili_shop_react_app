41b800f3bc6f1e9a6f4eeb85b9cd48c192dbc42e
Minh Tri <hominhtri135@gmail.com>
2023-08-25 23:50:41 +0700

    fix uuid

--- src/components/loading/CategoryLoading.js
+++ src/components/loading/CategoryLoading.js
@@ -8,7 +8,7 @@ const CategoryLoading = () => {
       <div className="mt-6 lg:col-span-4 lg:mt-0">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
           {new Array(6).fill(0).map((item) => (
-            <ProductItemLoading key={uuidv4}></ProductItemLoading>
+            <ProductItemLoading key={uuidv4()}></ProductItemLoading>
           ))}
         </div>
       </div>
